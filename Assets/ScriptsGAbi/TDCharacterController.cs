8a3e8235002ba38ac2d44fdb1dde2a477ee6bce2
MxPaint <jorgefm97@gmail.com>
2025-10-07 13:43:27 +0200

    Movimiento animado druida
    Hay mucho rojo porque he quitado el muñeco default

--- Assets/ScriptsGAbi/TDCharacterController.cs
+++ Assets/ScriptsGAbi/TDCharacterController.cs
@@ -10,9 +10,14 @@ namespace Cainos.PixelArtTopDown_Basic
         private Vector3 targetPosition;
         private bool isMoving = false;
 
+        private Animator anim;
+
         private void Start()
         {
             targetPosition = transform.position;
+            anim = GetComponent<Animator>();
+            anim.SetFloat("LastX", 0);
+            anim.SetFloat("LastY", -1);
         }
 
         private void Update()
@@ -31,21 +36,29 @@ namespace Cainos.PixelArtTopDown_Basic
         {
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
+                anim.SetFloat("LastX", 0);
+                anim.SetFloat("LastY", 1);
                 targetPosition += Vector3.up * gridSize;
                 isMoving = true;
             }
             else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
             {
+                anim.SetFloat("LastX", 0);
+                anim.SetFloat("LastY", -1);
                 targetPosition += Vector3.down * gridSize;
                 isMoving = true;
             }
             else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
+                anim.SetFloat("LastX", -1);
+                anim.SetFloat("LastY", 0);
                 targetPosition += Vector3.left * gridSize;
                 isMoving = true;
             }
             else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             {
+                anim.SetFloat("LastX", 1);
+                anim.SetFloat("LastY", 0);
                 targetPosition += Vector3.right * gridSize;
                 isMoving = true;
             }
@@ -55,11 +68,13 @@ namespace Cainos.PixelArtTopDown_Basic
         {
             if (!isMoving) return;
 
+            anim.SetBool("IsMoving", true);
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
 
             if (transform.position == targetPosition)
             {
-                isMoving = false; 
+                isMoving = false;
+                anim.SetBool("IsMoving", false);
             }
         }
     }
