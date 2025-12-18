5a820166834eaae3668e7d7518fcfbde97e71772
MxPaint <jorgefm97@gmail.com>
2025-12-18 22:49:29 +0100

    Button-Barrier

--- Assets/Scripts/TopDown/TDButton.cs
+++ Assets/Scripts/TopDown/TDButton.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class TDButton : MonoBehaviour
 {
+    public TDBarrier[] barriers;
+    public Sprite buttonOn;
+    public Sprite buttonOff;
+    public bool isOn = true;
+
+    private SpriteRenderer spRen;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spRen=GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -18,6 +25,23 @@ public class TDButton : MonoBehaviour
 
     public void Activate()
     {
-        Debug.Log("Button Activated");
+        //Debug.Log("Button Activated");
+        if(isOn)
+        {
+            isOn = false;
+            spRen.sprite = buttonOff;
+            for(int i = 0; i < barriers.Length; i++)
+            {
+                barriers[i].turnOff();
+            }
+        } else
+        {
+            isOn = true;
+            spRen.sprite = buttonOn;
+            for (int i = 0; i < barriers.Length; i++)
+            {
+                barriers[i].turnOn();
+            }
+        }
     }
 }
