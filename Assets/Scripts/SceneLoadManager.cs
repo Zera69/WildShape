45e195d7cbac778fea35f34b7c446e77c00b9954
Alex9332 <alexandreutormos@gmail.com>
2025-12-19 13:13:54 +0100

    Transiccion de Escenas
    Nuevo script SceneLoadManager con fade como corutina

---
+++ Assets/Scripts/SceneLoadManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoadManager : MonoBehaviour
+{
+    public Image imagenDeFondo;
+    public float duracion = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FadeFromBlack());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Active()
+    {
+        StartCoroutine(LoadWithFade());
+    }
+
+    IEnumerator LoadWithFade()
+    {
+        yield return FadeToBlack();
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    IEnumerator FadeFromBlack()
+    {
+        float tiempo = 0f;
+        Color color = imagenDeFondo.color;
+
+        while (tiempo < duracion)
+        {
+            tiempo += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, tiempo / duracion);
+            color.a = alpha;
+            imagenDeFondo.color = color;
+            yield return null;
+        }
+
+        color.a = 0f;
+        imagenDeFondo.color = color;
+    }
+
+    public IEnumerator FadeToBlack()
+    {
+        float tiempo = 0f;
+        Color color = imagenDeFondo.color;
+
+        while (tiempo < duracion)
+        {
+            tiempo += Time.deltaTime;
+            float alpha = Mathf.Lerp(0f, 1f, tiempo / duracion);
+            color.a = alpha;
+            imagenDeFondo.color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        imagenDeFondo.color = color;
+    }
+
+}
